cf051f17036fb18ac14362e168da1dc0bb366c98
TofuBoy05 <charlzfelipe02@gmail.com>
2024-01-15 21:40:14 +0800

    temporary tool for a tedious excel task lmao

---
+++ src/routes/tool/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+    let input;
+
+    function getLinesFromTextarea(textareaValue) {
+        // Split the textarea value by newline characters
+        const lines = textareaValue.split('\n');
+
+        // Filter out empty lines (if any)
+        const nonEmptyLines = lines.filter(line => line.trim() !== '');
+
+        return nonEmptyLines;
+    }
+
+    function rearrangeNames(namesArray) {
+        return namesArray.map(name => {
+            const [lastName, ...rest] = name.split(', ');
+            const formattedName = rest.join(' ');
+            return `${formattedName} ${lastName}`;
+        });
+    }
+
+    $: rearranged = input ? rearrangeNames(getLinesFromTextarea(input)) : '';
+</script>
+
+<div>
+    
+    <textarea class="textarea textarea-bordered h-52 w-full text-white mt-20" bind:value={input}></textarea>
+    
+    <br />
+    {#each rearranged as name}
+        <p>{name}</p>
+    {/each}
+
+</div>
